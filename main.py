d5429b538c52e728732025c9a8d0bb434d1c07f2
ThatWonderfulLife <pedroarena4@gmail.com>
2023-10-21 20:53:25 -0300

    Mudança para MySql e aplicação de Login em Server

--- main.py
+++ main.py
@@ -1,108 +1,103 @@
-import pyodbc as db
+import mysql.connector
 import json
-from flask import Flask,redirect,url_for,render_template,request, jsonify
-import pyodbc as db
+from flask import Flask,render_template,request,jsonify
 
-
-data=[]
-result =[]
-server = 'DESKTOP-0LIUFQV'
-database = 'python_dataset'
-username = 'Administrador'
-password = ''
-app = Flask(__name__)
-
-def sortData(rows):
-    for tuple in rows:
-        dict = {'name':tuple[0],
-            'platform':tuple[1],
-            'yearOfRelease':tuple[2],
-            'genre':tuple[3],
-            'publisher':tuple[4],
-            'NA_Sales':tuple[5],
-            'EU_Sales':tuple[6],
-            'JP_Sales':tuple[7]
-            }
-        data.append(dict)
-    return dict    
+mydb = mysql.connector.connect(
+  host="",
+  user="",
+  password="&",
+  database=""
+)
 
 def getData(sqlCommand):
-    conn = db.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';')
-    call = conn.cursor()
-    call.execute(f"{sqlCommand}")
-    rows = call.fetchall()
-    dict = sortData(rows)
-    data.append(dict)
-    call.close()
-    conn.close()
-
-    return data
-
-
-def postData(file):
+    data = []
     try:
-        conn = db.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';')
-        call = conn.cursor()
-        data = file
-        sqlCommand = f'''insert into dbo.dataset_import (Name,Platform,YearOfRelease,Genre,Publisher,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales,Critic_Score,Critic_Count,User_Score,Developer,Rating) VALUES ('{data["Name"]}','{data["Platform"]}',{data["YearOfRelease"]},'{data["Genre"]}','{data["Publisher"]}',{data["NA_Sales"]},{data["EU_Sales"]},{data["JP_Sales"]},{data["Other_Sales"]},{data["Global_Sales"]},{data["Critic_Score"]},{data["Critic_Count"]},{data["User_Score"]},'{data["Developer"]}',{data["Rating"]})'''
-        print(sqlCommand)
-        call.execute(f"{sqlCommand}")
-        call.close()
-        conn.close()
+        cursor = mydb.cursor()
+        cursor.execute(sqlCommand)
+        result = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+
+        for row in result:
+            data.append(dict(zip(columns, row)) if row else None)
+    except Exception as e:
+        data=[]
+    finally:
+        cursor.close()
+    return json.dumps(data)
+    
+def postData(data):
+    try:
+        cursor = mydb.cursor()
+        sqlCommand = f'''insert into mysql.Games (Name,Platform,Year,Genre,Publisher,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global,Critical_Score,Critical_Count,User_Score,User_Count,Developer,Rating) VALUES ('{data["Name"]}','{data["Platform"]}',{data["YearOfRelease"]},'{data["Genre"]}','{data["Publisher"]}',{data["NA_Sales"]},{data["EU_Sales"]},{data["JP_Sales"]},{data["Other_Sales"]},{data["Global_Sales"]},{data["Critic_Score"]},{data["Critic_Count"]},{data["User_Score"]},{data["User_Count"]},'{data["Developer"]}',{data["Rating"]});'''
+        cursor.execute(sqlCommand)
+        mydb.commit()
+
+        cursor.close()
+        mydb.close()
         return jsonify({"message": "Data added successfully", "Query":[sqlCommand]})
+    
     except Exception as e:
         return jsonify({"Erro": str(e), "Query":[sqlCommand]})
-    
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 @app.route("/getAllGames")
 def getAllGames():
-    return getData("select * from dbo.dataset_import")
+    data=json.loads(getData("select * from mysql.Games"))
+    return jsonify(data)
 
 @app.route("/getAllGamesByConsole/<console>", methods = ["GET"])
 def getAllGamesByConsole(console):
-    return getData(f"select * from dbo.dataset_import where Platform = '{console}'")
+    data=json.loads(getData(f"select * from mysql.Games where Platform = '{console}'"))
+    return jsonify(data)
 
 @app.route("/getAllByYear/<year>",methods = ["GET"])
 def getAllByYear(year):
-    return getData(f"select * from dbo.dataset_import where YearOfRelease = {year}")
+    data=json.loads(getData(f"select * from mysql.Games where Year = '{year}'"))
+    return jsonify(data)
 
 @app.route("/getAllByGenre/<genre>",methods = ["GET"])
 def getAllByGenre(genre):
-    return getData(f"select * from dbo.dataset_import where Genre = '{genre}'")
+    data=json.loads(getData(f"select * from mysql.Games where Genre = '{genre}'"))
+    return jsonify(data)
 
 @app.route("/getAllByPublisher/<publisher>",methods = ["GET"])
 def getAllByPublisher(publisher):
-    return getData(f"select * from dbo.dataset_import where publisher = '{publisher}'")
+    data=json.loads(getData(f"select * from mysql.Games where Publisher = '{publisher}'"))
+    return jsonify(data)
 
 @app.route("/getFromUserScore",methods = ["GET"])
 def getFromUserScore():
-    return getData(f"select * from dbo.dataset_import order by User_Score desc")
+    data=json.loads(getData(f"select * from mysql.Games order by User_Score desc"))
+    return jsonify(data)
 
 @app.route("/getFromCriticScore",methods = ["GET"])
 def getFromCriticScore():
-    return getData(f"select * from dbo.dataset_import order by Critic_Score desc")
+    data=json.loads(getData(f"select * from mysql.Games order by Critic_Score desc"))
+    return jsonify(data)
 
 @app.route("/getFromGlobalSales",methods = ["GET"])
 def getFromGlobalSales():
-    return getData(f"select * from dbo.dataset_import order by Critic_Score desc")
+    data=json.loads(getData(f"select * from mysql.Games order by Critic_Score desc"))
+    return jsonify(data)
 
 @app.route("/getFromDeveloper/<developer>",methods = ["GET"])
 def getFromDeveloper(developer):
-    return getData(f"select * from dbo.dataset_import where Developer = '{developer}'")
+    data=json.loads(getData(f"select * from mysql.Games where Developer = '{developer}'"))
+    return jsonify(data)
 
 @app.route("/getBestFromDeveloper/<developer>",methods = ["GET"])
 def getBestFromDeveloper(developer):
-    return getData(f"select * from dbo.dataset_import where Developer = '{developer}' order by User_Score desc")
+    data=json.loads(getData(f"select * from mysql.Games where Developer = '{developer}' order by User_Score desc"))
+    return jsonify(data)
 
 @app.route("/postNewGame",methods = ["POST"])
 def postNewGame():
-    file = request.get_json()
-    return postData(file)
-
+    return postData(request.get_json())
 
 if __name__ == "__main__":
-    app.run()
-
-
+    app.run(host='',port=9999, debug=True)
